df15b47c430184407fdaa0f91908aa7af3255140
BradBin <820280738@qq.com>
2019-05-31 00:00:27 +0800

    initialize

---
+++ KKOpenSSL_iOS/KKOpenSSL_iOS.h
@@ -0,0 +1,19 @@
+//
+//  KKOpenSSL_iOS.h
+//  KKOpenSSL_iOS
+//
+//  Created by Macbook Pro 15.4  on 2019/5/30.
+//  Copyright © 2019 Macbook Pro 15.4 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKOpenSSL_iOS.
+FOUNDATION_EXPORT double KKOpenSSL_iOSVersionNumber;
+
+//! Project version string for KKOpenSSL_iOS.
+FOUNDATION_EXPORT const unsigned char KKOpenSSL_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKOpenSSL_iOS/PublicHeader.h>
+
+
---
+++ KKOpenSSL_iOSTests/KKOpenSSL_iOSTests.m
@@ -0,0 +1,37 @@
+//
+//  KKOpenSSL_iOSTests.m
+//  KKOpenSSL_iOSTests
+//
+//  Created by Macbook Pro 15.4  on 2019/5/30.
+//  Copyright © 2019 Macbook Pro 15.4 . All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KKOpenSSL_iOSTests : XCTestCase
+
+@end
+
+@implementation KKOpenSSL_iOSTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
